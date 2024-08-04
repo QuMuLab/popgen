608226ff83ccdf544bd808d5a3f53994b218d952
Christian Muise <christian.muise@gmail.com>
2024-08-03 23:35:28 -0400

    Rewrote the hard-constraint part of the encoding for bauhaus.

--- encoder.py
+++ encoder.py
@@ -1,28 +1,29 @@
 
 import argparse
-from bauhaus import Encoding, proposition, constraint
+from bauhaus import Encoding, proposition, And, Or
 
 from lifter import lift_POP
 
 import tarskilite as tl
 
 
+class Hashable:
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, __value: object) -> bool:
+        return hash(self) == hash(__value)
+
+    def __repr__(self):
+        return str(self)
+
 def encode_POP(pop, cmdargs):
 
     # For sanitization, make sure we close the pop
     pop.transativly_close()
 
-    prob = tl.STRIPS(cmdargs.domain, cmdargs.problem)
-    allF = prob.fluents
-    allA = prob.actions
-    I = prob.init
-    G = prob.goal
-
-
     F = pop.F
     A = pop.A
-    I = pop.I
-    G = pop.G
 
     init = pop.init
     goal = pop.goal
@@ -40,125 +41,124 @@ def encode_POP(pop, cmdargs):
         for f in a.dels:
             deleters[f].add(a)
 
-    VARNUM = 1
+    E = Encoding()
 
-    # Create the vars for each action
-    v2a = {}
-    a2v = {}
-    for a in A:
-        v2a[VARNUM] = a
-        a2v[a] = VARNUM
-        VARNUM += 1
+    @proposition(E)
+    class Action(Hashable):
+        def __init__(self, name):
+            self.name = name
 
-    # Create the vars for each action ordering
-    v2o = {}
-    o2v = {}
-    for a1 in A:
-        for a2 in A:
-            v2o[VARNUM] = (a1,a2)
-            o2v[(a1,a2)] = VARNUM
-            VARNUM += 1
+        def __repr__(self):
+            return f"Action({self.name})"
 
-    # Create the vars for each possible action support
-    v2s = {}
-    s2v = {}
-    for a2 in A:
-        for p in a2.pres:
-            for a1 in adders[p]:
-                v2s[VARNUM] = (a1,p,a2)
-                s2v[(a1,p,a2)] = VARNUM
-                VARNUM += 1
+        def __str__(self):
+            return self.name
 
-    formula = OptimizedLevelWeightedFormula()
+        def __hash__(self) -> int:
+            return hash(self.name)
+
+    @proposition(E)
+    class Order(Hashable):
+        def __init__(self, a1, a2):
+            self.a1 = a1
+            self.a2 = a2
+
+        def __repr__(self):
+            return f"Order({self.a1}, {self.a2})"
+
+        def __str__(self):
+            return f"{self.a1} -> {self.a2}"
+
+    @proposition(E)
+    class Support(Hashable):
+        def __init__(self, a1, p, a2):
+            self.a1 = a1
+            self.p = p
+            self.a2 = a2
+
+        def __repr__(self):
+            return f"Support({self.a1}, {self.p}, {self.a2})"
+
+        def __str__(self):
+            return f"{self.a1} supports {self.p} for {self.a2}"
+
+    actions = [Action(a) for a in A]
+    orders = [Order(a1, a2) for a1 in A for a2 in A]
+    supports = [Support(a1, p, a2) for a2 in A for p in a2.pres for a1 in adders[p]]
+
+    v2a = {action: action.name for action in actions}
+    a2v = {action.name: action for action in actions}
+
+    v2o = {order: (order.a1, order.a2) for order in orders}
+    o2v = {(order.a1, order.a2): order for order in orders}
+
+    v2s = {support: (support.a1, support.p, support.a2) for support in supports}
+    # s2v = {(support.a1, support.p, support.a2): support for support in supports}
+
+    clauses = []
 
     # Add the antisymmetric ordering constraints
-    for a in A:
-        formula.addClause([-o2v[(a,a)]])
+    clauses.extend([~Order(a, a) for a in A])
 
     # Add the transitivity constraints
     for a1 in A:
         for a2 in A:
             for a3 in A:
-                formula.addClause([-o2v[(a1,a2)], -o2v[(a2,a3)], o2v[(a1,a3)]])
+                clauses.append((Order(a1, a2) & Order(a2, a3)) >> Order(a1, a3))
 
     # Add the ordering -> actions constraints
     for a1 in A:
         for a2 in A:
-            formula.addClause([-o2v[(a1,a2)], a2v[a1]])
-            formula.addClause([-o2v[(a1,a2)], a2v[a2]])
+            clauses.append(Order(a1, a2) >> (Action(a1) & Action(a2)))
 
     # Make sure everything comes after the init, and before the goal
     for a in A:
         if a is not init:
-            formula.addClause([-a2v[a], o2v[(init, a)]])
+            clauses.append(Action(a) >> Order(init, a))
         if a is not goal:
-            formula.addClause([-a2v[a], o2v[(a, goal)]])
+            clauses.append(Action(a) >> Order(a, goal))
 
     # Ensure that we have a goal and init action.
-    formula.addClause([a2v[init]])
-    formula.addClause([a2v[goal]])
+    clauses.append(Action(init))
+    clauses.append(Action(goal))
 
     # Satisfy all the preconditions
     for a2 in A:
-        for p in a2.precond:
-            formula.addClause([-a2v[a2]] + [s2v[(a1,p,a2)] for a1 in [x for x in adders[p] if x is not a2]])
+        for p in a2.pres:
+            clauses.append(Action(a2) >> Or([Support(a1, p, a2) for a1 in [x for x in adders[p] if x is not a2]]))
 
     # Create unthreatened support
     for a2 in A:
-        for p in a2.precond:
+        for p in a2.pres:
             for a1 in [x for x in adders[p] if x is not a2]:
 
                 # Support implies ordering
-                formula.addClause([-s2v[(a1,p,a2)], o2v[(a1,a2)]])
+                clauses.append(Support(a1, p, a2) >> Order(a1, a2))
 
                 # Forbid threats
-                #print "%s--%s-->%s: %s" % (str(a1), str(p), str(a2), str(deleters[p]))
-                for ad in [x for x in deleters[p] if x not in set([a1,a2])]:
-                    #print "...%s--%s-->%s: %s" % (str(a1), str(p), str(a2), str(ad))
-                    formula.addClause([-s2v[(a1,p,a2)], -a2v[ad], o2v[(ad,a1)], o2v[(a2,ad)]])
-
-    # Add the main constraints that satisfy preconditions without threats
-    #for a in A:
-        #total = []
-
-        #for p in a.precond:
-            #subtheories = []
-            #for achiever in adders[p]:
-                #if achiever is not a:
-                    #subtheory = [set([o2v[(achiever, a)]])]
-                    #for deleter in deleters[p]:
-                        #if deleter is not a:
-                            #subtheory.append(set([-a2v[deleter], o2v[(deleter, achiever)], o2v[(a, deleter)]]))
-                    #subtheories.append(subtheory)
-
-            #mastertheory = subtheories.pop()
-
-            #while subtheories:
-                #currenttheory = subtheories.pop()
-                #mastertheory = [i | j for i in mastertheory for j in currenttheory]
-
-            #total.extend(mastertheory)
-
-        #total = [item | set([-a2v[a]]) for item in total]
-
-        #for cls in total:
-            #assert cls.__class__ == set
-            #hard_clauses.append(cls)
+                for ad in deleters[p]:
+                    if ad not in [a1, a2]:
+                        clauses.append(Support(a1, p, a2) >> (~Action(ad) | Order(ad, a1) | Order(a2, ad)))
 
 
-    if 'SERIAL' in flags:
+    if cmdargs.serial:
         for a1 in A:
             for a2 in A:
                 if a1 is not a2:
-                    formula.addClause([-a2v[a1], -a2v[a2], o2v[(a1,a2)], o2v[(a2,a1)]])
+                    clauses.append((Action(a1) & Action(a2)) >> (Order(a1, a2) | Order(a2, a1)))
 
-    if 'ALLACT' in flags:
+    if cmdargs.allact:
         for a in A:
-            formula.addClause([a2v[a]])
+            clauses.append(Action(a))
 
-    if 'DEORDER' in flags:
+    if cmdargs.deorder:
         for (ai,aj) in pop.get_links():
-            formula.addClause([-o2v[(aj, ai)]])
+            clauses.append(~Order(aj, ai))
+
+    F = And(clauses).compile().simplify()
+    assert False, "Made it!"
+
+    # TODO: Figure out how to get the soft unit clauses into the patrially weighted CNF
 
     # Now add the soft clauses.
     for a1 in A:
